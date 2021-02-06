64eb2a8c413fbd23f1de01d812092813037f01c2
april <april@aprils-MacBook-Air.local>
2021-02-06 11:07:04 -0800

    Added html pages. Cleaned unneeded directories

--- blackwingshacks/user_data/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Employee
-
-# Create your views here.
-def user_page(request):
-    #return render(request, 'user_info/page1.html')
-    employees = Employee.objects.all().order_by('baseSalary') #Grabs all records from Employee database table
-    return render(request, 'user_data/user_page.html', {'employees' : employees})
--- cockroach_example/cockroach_example/urls.py
+++ cockroach_example/cockroach_example/urls.py
@@ -1,7 +1,7 @@
-"""blackwingshacks URL Configuration
+"""cockroach_example URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url, include
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
-    url('^admin/', admin.site.urls),
-    url(r'^user_data', include('user_data.urls')), # now django knows to look into urls in app_1
+    url('admin/', admin.site.urls),
+    url(r'^userPage/$', views.user_page), #about url    
     url(r'^about/$', views.about), #about url
     url(r'^$', views.homepage) # homepage url
 ]
